e982e53e2a77e25f19c0bd0d2b761b66db09348c
Tany CATALA BAILLY <catalatany@gmail.com>
2023-11-24 10:50:00 +0100

    commentaire sur controlerGestionDonnee

--- src/iut/sae/ihm/controleur/ControleurGestionDonnees.java
+++ src/iut/sae/ihm/controleur/ControleurGestionDonnees.java
@@ -39,133 +39,153 @@ import javafx.scene.control.TreeView;
  */
 public class ControleurGestionDonnees {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="btnAnnuler"
-    private Button btnAnnuler; // Value injected by FXMLLoader
+	@FXML // fx:id="btnAnnuler"
+	private Button btnAnnuler; // Value injected by FXMLLoader
 
-    @FXML // fx:id="treeViewData"
-    private TreeView<CustomBtn> treeViewData; // Value injected by FXMLLoader
+	@FXML // fx:id="treeViewData"
+	private TreeView<CustomBtn> treeViewData; // Value injected by FXMLLoader
 
-    @FXML
-    void retourArriere(ActionEvent event) {
-        EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_MENU_GESTION_DONNEES);
-    }
+	@FXML
+	void retourArriere(ActionEvent event) {
+		EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_MENU_GESTION_DONNEES);
+	}
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert btnAnnuler != null
-                : "fx:id=\"btnAnnuler\" was not injected: check your FXML file 'GestionDonnees.fxml'.";
-        assert treeViewData != null
-                : "fx:id=\"treeViewData\" was not injected: check your FXML file 'GestionDonnees.fxml'.";
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert btnAnnuler != null
+				: "fx:id=\"btnAnnuler\" was not injected: check your FXML file 'GestionDonnees.fxml'.";
+		assert treeViewData != null
+				: "fx:id=\"treeViewData\" was not injected: check your FXML file 'GestionDonnees.fxml'.";
 
-        // Créez les racines pour le TreeView
+		
+		// cree l'eventHandler pour modifier la Categorie
+		EventHandler<ActionEvent> modifierCat = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e)
+			{
+				//recupere la categorie qui etait dans le menuitem
+				Categorie laCategorie = (Categorie)((MenuItem)e.getSource()).getUserData();
+				//verifier que la categorie choisi n'est pas general
+				if (laCategorie != Donnees.listeCategorie.getFirst()) {
+					EchangeurDeVue.echangerAvec(laCategorie);
+				}else {
+					Alert erreur = new Alert(AlertType.WARNING);
+					erreur.setContentText("Vous ne pouvez pas modifier la categorie Général");
+					erreur.showAndWait();
+				}
 
-                EventHandler<ActionEvent> modifierCat = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                Categorie laCategorie = (Categorie)((MenuItem)e.getSource()).getUserData();
-                if (laCategorie != Donnees.listeCategorie.getFirst()) {
-                    EchangeurDeVue.echangerAvec(laCategorie);
-                }else {
-                    Alert erreur = new Alert(AlertType.WARNING);
-                    erreur.setContentText("Vous ne pouvez pas modifier la categorie Général");
-                    erreur.showAndWait();
-                }
+			}
+		};
+		//cree l'eventhandler pour modifier les question
+		EventHandler<ActionEvent> modifierQuest = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e)
+			{
+				//recupere la question qui est passé en parametre du menuitem
+				Question laQuestion = (Question) ((MenuItem)e.getSource()).getUserData();
+				EchangeurDeVue.echangerAvec(laQuestion);
+			}
+		};
+		//cree l'eventhandler pour suprimer les categorie
+		EventHandler<ActionEvent> suprimerCat = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e)
+			{
+				//recupere la categorie qui etait dans le menuitem
+				Categorie laCategorie = (Categorie)((MenuItem)e.getSource()).getUserData();
+				//verifie sur la categorie choisi n'est pas general
+				if (laCategorie != Donnees.listeCategorie.getFirst()) {
+					if (Donnees.isCategorieVide(laCategorie)) {
+						Donnees.suprimerCategorie(laCategorie);
+					}else {
+						//affiche l'alert que la categorie n'est pas vide
+						Alert confirmation = new Alert(AlertType.CONFIRMATION);
+						confirmation.setContentText("La categorie contien des question.\nCette action les suprimeras aussi.");
+						Optional<ButtonType> result = confirmation.showAndWait();
+						//si l'utilisateur clique sur ok, les question et la categorie son suprimé
+						if(result.get() == ButtonType.OK) {
+							Donnees.suprimerCategorie(laCategorie);
+						}
+					}
+					EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_GESTION_DONNEES);
 
-            }
-        };
-        EventHandler<ActionEvent> modifierQuest = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                Question laQuestion = (Question) ((MenuItem)e.getSource()).getUserData();
-                EchangeurDeVue.echangerAvec(laQuestion);
-            }
-        };
-        EventHandler<ActionEvent> suprimerCat = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                Categorie laCategorie = (Categorie)((MenuItem)e.getSource()).getUserData();
+				}else {
+					Alert erreur = new Alert(AlertType.WARNING);
+					erreur.setContentText("Vous ne pouvez pas suprimer la categorie Général");
+					erreur.showAndWait();
+				}
+			}
+		};
+		//cree l'eventHandler de supresion question
+		EventHandler<ActionEvent> suprimerQuest = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e)
+			{
+				//recupere la question qui est passé en parametre du menuitem
+				Question laQuestion = (Question) ((MenuItem)e.getSource()).getUserData();
+				Donnees.suprimerQuestion(laQuestion);
+				EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_GESTION_DONNEES);
+			}
+		};
 
+		//creation d'une arraylist qui contiendra les treeitem de categorie
+		List<TreeItem<CustomBtn>> listeTreeItem = new ArrayList<>();
+		for (Categorie item : Donnees.listeCategorie) {
+			//creation du menuButton de modification
+			MenuButton btnModifierCategorie = new MenuButton("Gérer");
+			MenuItem menuItemModifier = new MenuItem("Modifier");
+			MenuItem menuItemSuprimer = new MenuItem("Suprimer");
+			
+			//la categorie est mise en parametre des menuItem
+			menuItemModifier.setUserData(item);
+			menuItemSuprimer.setUserData(item);
+			//les eventHandler sont placer sur les menuItem
+			menuItemModifier.setOnAction(modifierCat);
+			menuItemSuprimer.setOnAction(suprimerCat);
+			//on ajoute les menuitem dans le menuButton
+			btnModifierCategorie.getItems().add(menuItemModifier);
+			btnModifierCategorie.getItems().add(menuItemSuprimer);
+			//ajout d'un treeitem avec en parametre la categorie et le menuButton
+			listeTreeItem.add(new TreeItem<CustomBtn>(
+					new CustomBtn(item, btnModifierCategorie)));
+		}
+		//pour chaque treeitem (donc pour chaque categorie)
+		for (TreeItem<CustomBtn> leTreeItem : listeTreeItem) {
+			//pour chaque question
+			for (Question laQuestion : Donnees.listeQuestions) {
+				//on recupere la categorie
+				Categorie cat = leTreeItem.getValue().getCategorie();
+				//si la categorie et la categorie de la question sont la meme
+				if (cat.toString().equals(laQuestion.getCategorie().toString())) {
+					//ajout du btn de modification de question
+					MenuButton btnModifierQuestion = new MenuButton("Gérer");
+					MenuItem menuItemModifier = new MenuItem("Modifier");
+					MenuItem menuItemSuprimer = new MenuItem("Suprimer");
+					//ajout de la question dans les menuItems
+					menuItemModifier.setUserData(laQuestion);
+					menuItemSuprimer.setUserData(laQuestion);
+					//on place les eventHandler
+					menuItemModifier.setOnAction(modifierQuest);
+					menuItemSuprimer.setOnAction(suprimerQuest);
+					//on ajoute les menuitem dans le menuButton
+					btnModifierQuestion.getItems().add(menuItemModifier);
+					btnModifierQuestion.getItems().add(menuItemSuprimer);
+					//on cree un treeitem avec en parametre la question et le menuButton
+					//et on l'ajoute au treeitem de la categorie
+					leTreeItem.getChildren().add(new TreeItem<CustomBtn>(
+							new CustomBtn(laQuestion, btnModifierQuestion)));
+				}
 
-                if (laCategorie != Donnees.listeCategorie.getFirst()) {
-                    if (Donnees.isCategorieVide(laCategorie)) {
-                        Donnees.suprimerCategorie(laCategorie);
-                    }else {
-                        Alert confirmation = new Alert(AlertType.CONFIRMATION);
-                        confirmation.setContentText("La categorie contien des question.\nCette action les suprimeras aussi.");
-                        Optional<ButtonType> result = confirmation.showAndWait();
-                        if(result.get() == ButtonType.OK) {
-                            Donnees.suprimerCategorie(laCategorie);
-                        }
-                    }
-                    EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_GESTION_DONNEES);
+			}
+		}
+		//on cree le root du treeview puis on le cache
+		treeViewData.setRoot(new TreeItem<>());
+		treeViewData.setShowRoot(false);
+		//on ajoute tout les treeitem des categorie
+		treeViewData.getRoot().getChildren().addAll(listeTreeItem);
 
-                }else {
-                    Alert erreur = new Alert(AlertType.WARNING);
-                    erreur.setContentText("Vous ne pouvez pas suprimer la categorie Général");
-                    erreur.showAndWait();
-                }
-            }
-        };
-        EventHandler<ActionEvent> suprimerQuest = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                Question laQuestion = (Question) ((MenuItem)e.getSource()).getUserData();
-                Donnees.suprimerQuestion(laQuestion);
-                EchangeurDeVue.echangerAvec(EnsembleDesVues.VUE_GESTION_DONNEES);
-            }
-        };
-
-        List<TreeItem<CustomBtn>> listeTreeItem = new ArrayList<>();
-        for (Categorie item : Donnees.listeCategorie) {
-            MenuButton btnModifierCategorie = new MenuButton("Gérer");
-            MenuItem menuItemModifier = new MenuItem("Modifier");
-            MenuItem menuItemSuprimer = new MenuItem("Suprimer");
-            menuItemModifier.setUserData(item);
-            menuItemSuprimer.setUserData(item);
-            menuItemModifier.setOnAction(modifierCat);
-            menuItemSuprimer.setOnAction(suprimerCat);
-            btnModifierCategorie.getItems().add(menuItemModifier);
-            btnModifierCategorie.getItems().add(menuItemSuprimer);
-            listeTreeItem.add(new TreeItem<CustomBtn>(new CustomBtn(item, btnModifierCategorie)));
-        }
-
-        for (TreeItem<CustomBtn> leTreeItem : listeTreeItem) {
-            for (Question laQuestion : Donnees.listeQuestions) {
-
-                Categorie cat = new Categorie("erreur");
-                for (Categorie laCategorie : Donnees.listeCategorie) {
-                    if (leTreeItem.getValue().getString() == laCategorie.toString()) {
-                        cat = laCategorie;
-                    }
-                }
-
-                if (cat.toString().equals(laQuestion.getCategorie().toString())) {
-                    MenuButton btnModifierQuestion = new MenuButton("Gérer");
-                    MenuItem menuItemModifier = new MenuItem("Modifier");
-                    MenuItem menuItemSuprimer = new MenuItem("Suprimer");
-                    menuItemModifier.setUserData(laQuestion);
-                    menuItemSuprimer.setUserData(laQuestion);
-                    menuItemModifier.setOnAction(modifierQuest);
-                    menuItemSuprimer.setOnAction(suprimerQuest);
-                    btnModifierQuestion.getItems().add(menuItemModifier);
-                    btnModifierQuestion.getItems().add(menuItemSuprimer);
-                    leTreeItem.getChildren()
-                    .add(new TreeItem<CustomBtn>(new CustomBtn(laQuestion, btnModifierQuestion)));
-                }
-
-            }
-        }
-
-        treeViewData.setRoot(new TreeItem<>());
-        treeViewData.setShowRoot(false);
-        treeViewData.getRoot().getChildren().addAll(listeTreeItem);
-
-    }
+	}
 
 }
