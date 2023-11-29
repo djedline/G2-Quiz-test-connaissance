d4161796794b24c890a64252548e2d5d030bbb89
Léo Cheikh-boukal <leo.ultimate34@gmail.com>
2023-11-29 14:27:21 +0100

    potentiel reglage pb reponse 5

--- src/iut/sae/ihm/controleur/ControleurCreerQuestion.java
+++ src/iut/sae/ihm/controleur/ControleurCreerQuestion.java
@@ -46,7 +46,7 @@ public class ControleurCreerQuestion {
     private Button btnCreer;
 
     @FXML
-    private TextField txtIntitule;
+    private TextArea txtIntitule;
 
     @FXML
     private TextField txtRepFausse1;
