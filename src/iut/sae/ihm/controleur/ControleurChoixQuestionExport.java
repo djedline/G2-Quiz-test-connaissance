136a76571d968def8bee8839c30fd49751713181
lbaudroit <109075845+lbaudroit@users.noreply.github.com>
2023-12-01 09:37:08 +0100

    Import retiré

--- src/iut/sae/ihm/controleur/ControleurChoixQuestionExport.java
+++ src/iut/sae/ihm/controleur/ControleurChoixQuestionExport.java
@@ -7,10 +7,8 @@ package iut.sae.ihm.controleur;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 
 import iut.sae.ihm.view.EchangeurDeVue;
 import iut.sae.ihm.view.EnsembleDesVues;
