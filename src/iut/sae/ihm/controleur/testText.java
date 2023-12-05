a7e65f2fa6586ca6e92d286758c19a65d9e2e46f
Djedline Boyer <djedline.boyer@iut-rodez.fr>
2023-12-05 17:03:49 +0100

    Temps d'attente serveur

---
+++ src/iut/sae/ihm/controleur/testText.java
@@ -0,0 +1,53 @@
+package iut.sae.ihm.controleur;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.function.UnaryOperator;
+
+public class testText extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        TextField textField = new TextField();
+        Label warningLabel = new Label("Veuillez entrer une valeur valide.");
+        warningLabel.setStyle("-fx-text-fill: red; -fx-font-weight: bold");
+
+        // Crée un TextFormatter avec un UnaryOperator pour valider la saisie
+        TextFormatter<String> textFormatter = new TextFormatter<>(new UnaryOperator<TextFormatter.Change>() {
+            @Override
+            public TextFormatter.Change apply(TextFormatter.Change change) {
+                String newText = change.getControlNewText();
+
+                // Exemple de condition : la saisie doit contenir au moins 5 caractères
+                if (newText.length() >= 5) {
+                    warningLabel.setVisible(false);
+                    return change;
+                } else {
+                    warningLabel.setVisible(true);
+                    return change;
+                }
+            }
+        });
+
+        textField.setTextFormatter(textFormatter);
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(textField, warningLabel);
+
+        Scene scene = new Scene(root, 300, 150);
+        primaryStage.setTitle("TextField Validation Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/iut/sae/modele/reseau/Serveur.java
+++ src/iut/sae/modele/reseau/Serveur.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Classe qui représente le serveur lors d'un échange d'information
@@ -17,90 +21,92 @@ import java.net.UnknownHostException;
  */
 public class Serveur {
 
-    private static final File FICHIER_A_ENVOYER = 
-            new File("src/iut/sae/modele/reseau/tests/fichEnvoi.txt");
+	private static final File FICHIER_A_ENVOYER = 
+			new File("src/iut/sae/modele/reseau/tests/fichEnvoi.txt");
 
-    /** socket de connexion lors du démarrage du client et serveur */
-    public ServerSocket conn;
+	/** socket de connexion lors du démarrage du client et serveur */
+	public ServerSocket conn;
 
-    /** socket qui permet la communication entre le serveur et le client */
-    private Socket comm;
-    
-    /**
-     * Utilitaires réseaux pour envoyer et recevoir facilement les chaînes
-     * de caractères. Lié à la socket.
-     */
-    private ReseauUtils util;
+	/** socket qui permet la communication entre le serveur et le client */
+	private Socket comm;
 
-    /**
-     * prépare le serveur en démarrant la socket conn.
-     * @throws IOException si la socket serveur ne peut être crée
-     */
-    public Serveur() throws IOException {
-        System.out.println("CREATION DU SERVEUR");
-        try {
-            conn = new ServerSocket(6666);
-            System.out.println("coucou");
-        } catch (UnknownHostException e) {
-            throw new IOException("Impossible de trouver l'adresse IP.", e);
-        } catch (IOException e) {
-            throw new IOException("Impossible de créer la Socket serveur.", e);
-        }
-    }
-    
-    /** 
-     * Envoie les données initiales (P et G de Diffie-Hellman)
-     * @return int
-     * @throws IOException 
-     * @throws InterruptedException 
-     */
-    public int envoiDonneesInitiale() throws IOException, InterruptedException {
-        int p = DiffieHellman.genererModulo();
-        int g = DiffieHellman.genererGenerateur();
-        String msgP = Integer.toString(p);
-        String msgG = Integer.toString(g);
-        int b = DiffieHellman.genererX();
-        
-        System.out.println("Envoi de G : ");
-        util.envoyerMessage(msgG);
-        Thread.sleep(1000);
-        System.out.println("Envoi de P : ");
-        util.envoyerMessage(msgP);
-        
-        String msgGB = Integer.toString((int) Math.pow(g, b));
-        Thread.sleep(1000);
-        System.out.println("Envoi de GB : ");
-        util.envoyerMessage(msgGB);
-        
-        System.out.println("Réception de GA : ");
-        String msgGA = util.reception();
-        try {
-        	int gA = Integer.parseInt(msgGA);
-        	int cle = (int) Math.pow(gA, b);
-        	System.out.println("Clé générée : " + cle);
-            return cle;
-        } catch (NumberFormatException e) {
-        	throw new IOException("Données corrompues envoyées par le client.");
-        }
-    }
-    
-    /** 
-     * Receptionne le contenu d'un fichier
-     * @param cle
-     * @return f
-     * @throws IOException
-     */
-    public String receptionFichier(int cle) throws IOException {
-    	String contenuFichCrypte = util.reception();
-    	return Cryptage.dechiffrer(contenuFichCrypte, 
-    			Integer.toString(cle));
-    }
-    
-    /*
-     * 
-     * @param args
-     * @throws InterruptedException
-     *
+	private ExecutorService executor;
+
+	/**
+	 * Utilitaires réseaux pour envoyer et recevoir facilement les chaînes
+	 * de caractères. Lié à la socket.
+	 */
+	private ReseauUtils util;
+
+	/**
+	 * prépare le serveur en démarrant la socket conn.
+	 * @throws IOException si la socket serveur ne peut être crée
+	 */
+	public Serveur() throws IOException {
+		System.out.println("CREATION DU SERVEUR");
+		try {
+			conn = new ServerSocket(6666);
+			System.out.println("coucou");
+		} catch (UnknownHostException e) {
+			throw new IOException("Impossible de trouver l'adresse IP.", e);
+		} catch (IOException e) {
+			throw new IOException("Impossible de créer la Socket serveur.", e);
+		}
+	}
+
+	/** 
+	 * Envoie les données initiales (P et G de Diffie-Hellman)
+	 * @return int
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 */
+	public int envoiDonneesInitiale() throws IOException, InterruptedException {
+		int p = DiffieHellman.genererModulo();
+		int g = DiffieHellman.genererGenerateur();
+		String msgP = Integer.toString(p);
+		String msgG = Integer.toString(g);
+		int b = DiffieHellman.genererX();
+
+		System.out.println("Envoi de G : ");
+		util.envoyerMessage(msgG);
+		Thread.sleep(1000);
+		System.out.println("Envoi de P : ");
+		util.envoyerMessage(msgP);
+
+		String msgGB = Integer.toString((int) Math.pow(g, b));
+		Thread.sleep(1000);
+		System.out.println("Envoi de GB : ");
+		util.envoyerMessage(msgGB);
+
+		System.out.println("Réception de GA : ");
+		String msgGA = util.reception();
+		try {
+			int gA = Integer.parseInt(msgGA);
+			int cle = (int) Math.pow(gA, b);
+			System.out.println("Clé générée : " + cle);
+			return cle;
+		} catch (NumberFormatException e) {
+			throw new IOException("Données corrompues envoyées par le client.");
+		}
+	}
+
+	/** 
+	 * Receptionne le contenu d'un fichier
+	 * @param cle
+	 * @return f
+	 * @throws IOException
+	 */
+	public String receptionFichier(int cle) throws IOException {
+		String contenuFichCrypte = util.reception();
+		return Cryptage.dechiffrer(contenuFichCrypte, 
+				Integer.toString(cle));
+	}
+
+	/*
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 *
     public void main(String[] args) throws InterruptedException {
         preparerServeur();
         accepterConnexion(); // bloquante : attend que le client se connecte
@@ -116,7 +122,7 @@ public class Serveur {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            
+
             //recu = recevoirMessage(FICHIER_RECEPTION, cle);
         }
 
@@ -135,41 +141,55 @@ public class Serveur {
     }*/
 
 
-    /**
-     * Ferme le serveur
-     */
-    public void fermetureServeur() {
-        try {
-        	if (comm != null) {
-        		comm.close();
-        	}
-        	if (conn != null) {
-        		conn.close();
-        	}
-        } catch (IOException e) {
-            System.err.println("Impossible de fermer la socket serveur.");
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Ferme le serveur
+	 */
+	public void fermetureServeur() {
+		try {
+			if (comm != null) {
+				comm.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (IOException e) {
+			System.err.println("Impossible de fermer la socket serveur.");
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * attend qu'un client demande une connexion et l'accepte
-     */
-    public void accepterConnexion() {
+	/**
+	 * attend qu'un client demande une connexion et l'accepte
+	 * @return 
+	 */
+	public boolean accepterConnexion(int timeout) {
 
-        System.out.println("ACCEPTATION");
-        try {
-            comm = conn.accept();
-            util = new ReseauUtils(comm);
-            System.out.println("La inet Adress conn : " 
-                                + conn.getInetAddress());
-            System.out.println("La inet Adress comm : " 
-                                + comm.getInetAddress());
-        } catch (IOException e) {
-            System.err.println("Impossible d'accepter la connection.");
-            e.printStackTrace();
-        }
-    }
-    
+		System.out.println("ACCEPTATION");
+		try {
+			executor = Executors.newSingleThreadExecutor();
 
+			executor.submit(() -> {
+				try {
+					comm = conn.accept();
+					util = new ReseauUtils(comm);
+					System.out.println("La inet Adress conn : " 
+							+ conn.getInetAddress());
+					System.out.println("La inet Adress comm : " 
+							+ comm.getInetAddress());
+				} catch (IOException e) {
+					System.err.println("Impossible d'accepter la connection.");
+					//e.printStackTrace();
+				}
+			}).get(timeout, TimeUnit.SECONDS);
+			return true;
+		} catch (Exception e) {
+			System.err.println("La connexion n'a pas pu être établie dans le délai spécifié.");
+			//e.printStackTrace();
+			return false;
+		} finally {
+			if (executor != null) {
+				executor.shutdownNow(); // Arrête le thread après l'exécution
+			}
+		}
+	}
 }
